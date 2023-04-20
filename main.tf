6bb7d6d5cd5031ee371a2fcaafff3b512b550fe6
windows 10 <dimpulsunayana205@gmail.com>
2023-04-20 22:16:19 +0530

    updated user repo

--- main.tf
+++ main.tf
@@ -24,4 +24,33 @@ resource "aws_security_group" "app" {
   )
 }
 
+resource "aws_launch_template" "main" {
+  name_prefix   = "${var.env}-${var.component}-template"
+  image_id      = data.aws_ami
+  instance_type = "t2.micro"
+}
+
+resource "aws_autoscaling_group" "asg" {
+  name                      = "${var.env}-${var.component}-asg"
+  max_size                  = var.max_size
+  min_size                  = var.min_size
+  desired_capacity          = var.desired_capacity
+  force_delete              = true
+  vpc_zone_identifier       = var.subnet_ids
+
+  launch_template {
+    id      = aws_launch_template.main.id
+    version = "$Latest"
+  }
+
+dynamic "tag" {
+  for_each = local.all_tags
+  content {
+    key = tag.value.key
+    value = tag.value.value
+    propagate_at_launch = true
+  }
+}
+}
+
 
--- var.tf
+++ var.tf
@@ -1,8 +1,12 @@
 variable "env" {}
-#variable "subnet_ids" {}
+variable "subnet_ids" {}
 variable "allow_cidr" {}
 variable "main_vpc" {}
 #variable "internal" {}
 #variable "subnets_name" {}
 variable "app_port" {}
 variable "component" {}
+variable "max_size" {}
+variable "min_size" {}
+variable "desired_capacity" {}
+variable "instance_type" {}
